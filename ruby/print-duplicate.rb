875f1ffc250bec5694cabc4c0a278c1a89e3e67c
Christopher Wright <christopherwright@christophersmbp.lan>
2019-06-01 23:59:22 -0400

    Print duplicate characters in string

---
+++ ruby/print-duplicate.rb
@@ -0,0 +1,13 @@
+duplicate_letters = ARGV[0]
+
+letter_map = {}
+
+duplicate_letters.split('').each do |char|
+    letter_map[char] = letter_map[char].to_i + 1
+end
+
+letter_map.each do |k,v|
+    if v > 1
+        puts %Q(#{k}: #{v})
+    end
+end
